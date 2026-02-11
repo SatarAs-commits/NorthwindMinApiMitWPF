956b1022e9983f052be0b9172c1f9baa23aef581
SatarAs-commits <AsfandyariS22137@sus.htl-grieskirchen.at>
2026-02-11 19:29:22 +0100

    Backend: Die Routes `GET: employees, customers, products, orders?, orderdetails?`, `POST: orders und orderdetails` und DELETE: orders

--- NorthwindMinApiMitWPF/Dtos/ProgramYourDtosInThisFolder.cs
+++ NorthwindMinApiMitWPF/Dtos/ProgramYourDtosInThisFolder.cs
@@ -1,4 +1,32 @@
 namespace NorthwindMinApiMitWPF.Dtos;
 //public record EmployeeDto(int EmployeeId, string FirstName, string LastName);
-public record EmployeeDto();
+public record EmployeeDto(
+  int EmployeeId,
+  string FirstName, 
+  string LastName);
 
+public record CustomerDto(
+  string ContactName);
+
+public record ProductDto(
+  string ProductName);
+public record OrderDto(
+  int  OrderId, 
+  String OrderDate, 
+  Boolean IsShipped, 
+  int NrItems);
+public record OrderDetailDto(
+  int OrderId, 
+  String Product, 
+  String Category, 
+  int Quantity, 
+  decimal UnitPrice);
+
+public record OrderPostDto(
+  int EmployeeId, 
+  String CustomerId);
+
+public record OrderdetailPostDto(
+  int OrderId,
+  int ProductId,
+  int Amount);
--- NorthwindMinApiMitWPF/Program.cs
+++ NorthwindMinApiMitWPF/Program.cs
@@ -1,8 +1,3 @@
-//----------------------------------------
-// .Net Core WebApi project create script 
-//           v10.0.4 from 2026-02-04
-//   (C)Robert Grueneis/HTL Grieskirchen 
-//----------------------------------------
 
 using GrueneisR.RestClientGenerator;
 
@@ -70,13 +65,101 @@ app.MapDbTests();
 app.MapGet("/employees", (NORTHWNDContext _db) =>
 {
   return _db.Employees
-    //.Select(x => new EmployeeDto(
-    //  x.EmployeeId,
-    //  x.FirstName,
-    //  x.LastName
-    //  ))
-    .Select(x => new EmployeeDto().CopyFrom(x))
+    //.Select(x => x.TransformTo<EmployeeDto>());
+    .Select(x => new EmployeeDto(
+      x.EmployeeId,
+      x.FirstName,
+      x.LastName
+      ))
+    ////.Select(x => new EmployeeDto().CopyFrom(x))
     .ToList();
+});
+
+app.MapGet("/customers", (NORTHWNDContext _db) =>
+{
+  return _db.Customers
+    .Select(x => x.TransformTo<CustomerDto>());
+});
+
+app.MapGet("/products", (NORTHWNDContext _db) =>
+{
+  return _db.Products
+    .Select(x => x.TransformTo<ProductDto>());
+});
+
+app.MapGet("/orders", (int EmployeeId, String CustomerId, NORTHWNDContext _db) =>
+{
+  return _db.Orders
+    .Where(x => x.EmployeeId == EmployeeId && x.CustomerId == CustomerId)
+    .Select(x => new OrderDto(
+      x.OrderId,
+      x.OrderDate.ToString(),
+      x.ShippedDate != null,
+      x.OrderDetails.Count))
+    .ToList();
+});
+
+app.MapGet("/orderdetails", (int OrderId, NORTHWNDContext _db) =>
+{
+  return _db.OrderDetails
+    .Where(x => x.OrderId == OrderId)
+    .Select(x => new OrderDetailDto(
+      x.OrderId,
+      x.Product.ProductName,
+      x.Product.Category.CategoryName,
+      x.Quantity,
+      (int)x.UnitPrice))
+    .ToList();
+});
+
+app.MapPost("/orders", (OrderPostDto dto, NORTHWNDContext _db) =>
+{
+  var newOrder = new Order
+  {
+    EmployeeId = dto.EmployeeId,
+    CustomerId = dto.CustomerId,
+  };
+
+  _db.Orders.Add(newOrder);
+  _db.SaveChanges();
+
+  return Results.Created($"/orders/{newOrder.OrderId}", newOrder.TransformTo<OrderDto>());
+});
+
+app.MapPost("/orderdetails", (OrderdetailPostDto dto, NORTHWNDContext _db) =>
+{
+  var product = _db.Products
+    .FirstOrDefault(x => x.ProductId == dto.ProductId);
+
+  if(product == null) { return Results.NotFound("Produkt nicht gefunden."); }
+
+  var orderdetail = new OrderDetail
+  {
+    OrderId = dto.OrderId,
+    ProductId = dto.ProductId,
+    Quantity = (short)dto.Amount,
+    UnitPrice = (decimal)product.UnitPrice,
+  };
+
+  _db.OrderDetails.Add(orderdetail);
+  _db.SaveChanges();
+
+  return Results.Ok();
+});
+
+app.MapDelete("/orders/{id}", (int id, NORTHWNDContext _db) =>
+{
+  var order = _db.Orders
+    .Include(x => x.OrderDetails)
+    .FirstOrDefault(x => x.OrderId == id);
+
+  if (order == null) { return Results.NotFound($"Bestellung {id} wurde nicht gefunden."); }
+
+  _db.OrderDetails.RemoveRange(order.OrderDetails);
+  _db.Orders.Remove(order);
+  _db.SaveChanges();
+
+  return Results.NoContent();
 });
 
 Console.WriteLine($"Ready for clients at {DateTime.Now:HH:mm:ss} ...");
